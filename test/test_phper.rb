784784a97d6ca4ac009bd32f39d3b83fffe4f0fe
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-08-11 13:51:34 +0900

    add tests

--- test/test_phper.rb
+++ test/test_phper.rb
@@ -1,12 +1,25 @@
 require 'helper'
 
 class TestPhper < Test::Unit::TestCase
-  should "version reterns version" do
-    require "stringio"
-    out = StringIO.new
-    Phper::CLI.new(out) { |cli|
-      cli.version
-      assert out.string =~ /\d+\.\d+.\d+/
-    }
+  def command commands
+    Phper::CLI::run(commands,@out)
+  end
+  context "commands" do
+    setup do
+      require "stringio"
+      @out = StringIO.new
+      @cli = Phper::CLI.new(@out)
+    end
+
+    should "version shows version" do
+      @cli.version
+      assert @out.string =~ /\d+\.\d+.\d+/
+    end
+
+    should "help shows help" do
+      command "help"
+      assert @out.string.length > 481
+    end
+
   end
 end
